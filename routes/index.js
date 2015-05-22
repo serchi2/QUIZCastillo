04953f8121cad24c41d11d2fed17fe927fb11748
serchi2 <sergio.csolana@gmail.com>
2015-05-22 10:34:19 +0200

    Registro de usuarios y gestion de cuentas

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,30 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
 var sessionController = require('../controllers/session_controller');
+var userController = require('../controllers/user_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: [] });
 });
 router.param('quizId', quizController.load); //autoload quiz:id
-router.param('commentId', commentController.load);
+router.param('commentId', commentController.load);//autoload :conmmentId
+router.param('userId', userController.load); //autoload: userId
 
 //definicion de rutas de sesion
 
 router.get('/login',  sessionController.new);     // formulario login
 router.post('/login', sessionController.create);  // crear sesión
-router.get('/logout', sessionController.destroy); // destruir sesión
+router.get('/logout', sessionController.destroy); // destruir sesión¿autologout??
+
+//definicion de rutas de cuenta
+
+   router.get('/user',  userController.new);     // formulario sign un
+   router.post('/user',  userController.create);     // registrar usuario
+   router.get('/user/:userId(\\d+)/edit',  sessionController.loginRequired, userController.edit);     // editar información de cuenta
+   router.put('/user/:userId(\\d+)',  sessionController.loginRequired, userController.update);     // actualizar información de cuenta
+   router.delete('/user/:userId(\\d+)',  sessionController.loginRequired, 				   userController.destroy);     // borrar cuenta
+
 
 router.get('/quizes',                      quizController.index);
 router.get('/quizes/:quizId(\\d+)',        quizController.show);
