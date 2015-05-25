5ba4656e2d6dbed16a62d479d66b4561d4ac8e5d
serchi2 <sergio.csolana@gmail.com>
2015-05-25 12:10:38 +0200

    Cajetin de Busqueda

--- routes/index.js
+++ routes/index.js
@@ -45,5 +45,8 @@ router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
 router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
 	                                    sessionController.loginRequired, commentController.ownershipRequired, commentController.publish);
+//buscador de preguntas
+ //router.get('/quizes?search', quizController.index);
+
 
 module.exports = router;
